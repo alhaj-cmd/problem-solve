60e6a3d4e89820d4bb7674d8ab41ebd5ba7e8a9d
github_username <you@domain.com>
2022-08-10 21:12:38 -0700

    first commit

---
+++ filename.js
@@ -0,0 +1,14 @@
+var strSplit = ['index.js','index.png','index.rumi.js']
+function findLongestWord(str) {
+   //var strSplit = str.split(' ');
+   var index = true;
+   for(var i = 0; i < strSplit.length; i++){
+   //   if(strSplit[i].length > longestWord){
+   //  longestWord = strSplit[i].length;
+   //    }
+   console.log(str[i]);
+   }
+   return index;
+ }
+ //findLongestWord("The quick brown fox jumped over the lazy dog");
+ console.log(findLongestWord(strSplit));
